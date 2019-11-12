607832275acbf80dce9e51eecf77dd11ad34fe66
Robert Gordon <Komeuppance@gmail.com>
2019-11-12 15:25:51 -0800

    First stretch goal complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 const Movie = (props) => {
@@ -31,28 +31,10 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
-  const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
       
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
+      <MovieCard key={movie.id} movie={movie} />
 
     </div>
   );
